0c56d3c5b2fd6434b20f1c8b935f212ceab06055
Retiro - 02 <Retiro - 02@DESKTOP-55DUIF2>
2019-07-01 16:23:01 -0300

    Mais ajustes

--- src/com/tvcaxias/testes/PlayTest.java
+++ src/com/tvcaxias/testes/PlayTest.java
@@ -1,10 +1,11 @@
 package com.tvcaxias.testes;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
 import java.io.File;
+import java.io.FileInputStream;
 
 import javax.swing.JOptionPane;
+
+import javazoom.jl.player.Player;
 
 public class PlayTest {
 	
@@ -14,20 +15,28 @@ public class PlayTest {
 			
 			//String endereco = "D:\\Server TV Caxias\\Fabiano Henrique\\Rádio\\Testes/abc.mp3";
 			//String endereco = "D:\\Server TV Caxias\\Fabiano Henrique\\Rádio\\Testes/Rádio.url_1.mp3";
-			String endereco = "D:\\Server TV Caxias\\Fabiano Henrique\\Rádio\\Testes/abc.wav";
+			//String endereco = "D:\\Server TV Caxias\\Fabiano Henrique\\Rádio\\Testes/abc.wav";
 			//String endereco = "D:\\Server TV Caxias\\Fabiano Henrique\\Rádio\\Testes/Rádio.url.wav";
+			String endereco = "C:\\abc.mp3";
 			File arquivo = new File(endereco);
 			
 //			AudioFileFormat a = AudioSystem.getAudioFileFormat(arquivo);
 //			System.out.println(a.getProperty("duration"));
 //			System.out.println(a.properties().isEmpty());
 			
-			AudioClip a = Applet.newAudioClip(arquivo.toURI().toURL());
+			//AudioClip a = Applet.newAudioClip(arquivo.toURI().toURL());
 			//a.play();
-			a.loop();
+			//a.loop();
 			
-//			Player p = new Player(new FileInputStream(arquivo));
-//			p.play();
+			while (true) {
+				new Thread() {
+					public void run() {
+						JOptionPane.showMessageDialog(null, "Esta música vai tocar pra sempre");
+					};
+				}.start();
+				Player p = new Player(new FileInputStream(arquivo));
+				p.play();
+			}
 			
 			/*Calendar c = new GregorianCalendar();
 			c.setTimeInMillis(a.getFrameLength());
@@ -41,11 +50,12 @@ public class PlayTest {
 //				Thread.sleep(2000);
 //			} while (a.);
 
-			System.out.println("...");
-			JOptionPane.showMessageDialog(null, "Esta música vai tocar pra sempre");
+			//System.out.println("...");
+			//JOptionPane.showMessageDialog(null, "Esta música vai tocar pra sempre");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage() + "\n\n************************************************\n\n" + e);
 		}
 		
 		
